0efee81174acb1e442f1bf623fda166b4228b975
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-17 14:46:37 +0100

    refactor: replace hardcoded strings with ROUTES constants in CreateGroupMemberPage

--- client/src/pages/CreateGroupMemberPage/CreateGroupMemberPage.jsx
+++ client/src/pages/CreateGroupMemberPage/CreateGroupMemberPage.jsx
@@ -1,58 +1,49 @@
-// React and Third-Party Libraries
 import React from "react";
 import { useTranslation } from "react-i18next";
-
-// Hooks
 import useGetPreviousRoutesFromLocalStorage from "../../hooks/useGetPreviousRouteFromLocalStorage";
 import useTriggerRerender from "../../hooks/useTriggerRerender";
-
-// Components
 import HelmetMetaTagsNetlify from "../../components/common/HelmetMetaTagsNetlify/HelmetMetaTagsNetlify";
 import PiratePx from "../../components/common/PiratePx/PiratePx";
 import InAppNavigationBar from "../../components/common/InAppNavigation/InAppNavigationBar/InAppNavigationBar";
 import CreateGroupMemberForm from "../../components/features/CreateGroupMember/CreateGroupMemberForm/CreateGroupMemberForm";
 import RenderGroupMemberNames from "../../components/features/CreateGroupMember/RenderGroupMemberNames/RenderGroupMemberNames";
-
-// Styles
+import { ROUTES } from "../../constants/routesConstants";
 import styles from "./CreateGroupMemberPage.module.css";
 
-function CreateGroupMemberPage() {
+const CreateGroupMemberPage = () => {
   const { previousRoute, isRetrieved } = useGetPreviousRoutesFromLocalStorage();
   const { t } = useTranslation();
 
-  // Check if current user is a new user, ie is redirected from onboarding-create-group route
+  // CODECHANGE: Replaced hardcoded strings with ROUTES constants
   const isNewUser =
     previousRoute && isRetrieved
-      ? previousRoute.includes("/onboarding-create-group")
+      ? previousRoute.includes(ROUTES.ONBOARDING.CREATE_GROUP)
       : false;
 
-  // Check if current user has created a new group within the application, ie is redirected from mangage-groups route
   const isInAppGroupCreation =
     previousRoute && isRetrieved
-      ? previousRoute.includes("/manage-groups")
+      ? previousRoute.includes(ROUTES.MANAGE_GROUPS)
       : false;
 
-  // Check if current user is redirected from main application
   const previousRouteExists = localStorage.getItem("previousRoute");
   const isRegularUser = !previousRouteExists;
 
-  // Custom hook to get groupCode and trigger rerender logic
   const { groupCode, rerenderTrigger, incrementRerenderTrigger } =
     useTriggerRerender();
 
   return (
     <main>
       <HelmetMetaTagsNetlify title={t("create-group-members-page-title")} />
-      <PiratePx COUNT_IDENTIFIER={"create-group-members"} />
+      <PiratePx COUNT_IDENTIFIER='create-group-members' />
       <InAppNavigationBar
-        abort={isNewUser || isInAppGroupCreation ? true : false}
-        abortRoute={isInAppGroupCreation ? "/manage-groups" : "/"}
-        back={isRegularUser ? true : false}
-        forward={isNewUser || isInAppGroupCreation ? true : false}
-        forwardRoute='/onboarding-group-settings'
+        abort={isNewUser || isInAppGroupCreation}
+        abortRoute={isInAppGroupCreation ? ROUTES.MANAGE_GROUPS : ROUTES.HOME}
+        back={isRegularUser}
+        forward={isNewUser || isInAppGroupCreation}
+        forwardRoute={ROUTES.ONBOARDING.GROUP_SETTINGS}
       />
       <div className={styles.addGroupMember}>
-        {!isNewUser ? <h1>{t("create-group-members-page-header")}</h1> : null}
+        {!isNewUser && <h1>{t("create-group-members-page-header")}</h1>}
         <h2>{t("create-group-members-form-header")}</h2>
         <CreateGroupMemberForm
           incrementRerenderTrigger={incrementRerenderTrigger}
@@ -69,5 +60,6 @@ function CreateGroupMemberPage() {
       </div>
     </main>
   );
-}
+};
+
 export default CreateGroupMemberPage;
