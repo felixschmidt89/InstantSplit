ca402d724d685472a308ed2d5fb152541eca643c
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-17 22:04:57 +0100

    refactor: apply ROUTES constant to OnboardingGroupSettingsPage

--- client/src/pages/OnboardingGroupSettingsPage/OnboardingGroupSettingsPage.jsx
+++ client/src/pages/OnboardingGroupSettingsPage/OnboardingGroupSettingsPage.jsx
@@ -1,15 +1,8 @@
-// React and Third-Party Libraries
 import React from "react";
 import { useTranslation } from "react-i18next";
-
-// Constants and Utils
 import { devLog } from "../../utils/errorUtils";
-
-// Hooks
 import useFetchGroupData from "../../hooks/useFetchGroupData";
 import useGetPreviousRoutesFromLocalStorage from "../../hooks/useGetPreviousRouteFromLocalStorage";
-
-// Components
 import HelmetMetaTagsNetlify from "../../components/common/HelmetMetaTagsNetlify/HelmetMetaTagsNetlify";
 import PiratePx from "../../components/common/PiratePx/PiratePx";
 import InAppNavigationBar from "../../components/common/InAppNavigation/InAppNavigationBar/InAppNavigationBar";
@@ -17,13 +10,9 @@ import ChangeGroupCurrency from "../../components/features/GroupSettings/ChangeG
 import ChangeDataPurgeSetting from "../../components/features/GroupSettings/ChangeDataPurgeSetting/ChangeDataPurgeSetting";
 import Spinner from "../../components/common/Spinner/Spinner";
 import GroupCodeSecurity from "../../components/features/GroupSettings/GroupCodeSecurity/GroupCodeSecurity";
-
-// Styles
+import { ROUTES } from "../../constants/routesConstants";
 import styles from "./OnboardingGroupSettingsPage.module.css";
 
-/**
- * Page component for rendering group currency & data purge settings during group creation process
- */
 const OnboardingGroupSettingsPage = () => {
   const groupCode = localStorage.getItem("activeGroupCode");
   const { groupData, isFetched } = useFetchGroupData(groupCode);
@@ -34,31 +23,33 @@ const OnboardingGroupSettingsPage = () => {
       <HelmetMetaTagsNetlify
         title={t("onboarding-group-settings-page-title")}
       />
-      <PiratePx COUNT_IDENTIFIER={"onboarding-group-settings"} />
+      <PiratePx COUNT_IDENTIFIER='onboarding-group-settings' />
       <InAppNavigationBar
-        back={true}
-        backRoute={"/create-group-members"}
-        forward={true}
-        forwardRoute='/instant-split'
+        back
+        backRoute={ROUTES.MEMBERS.CREATE}
+        forward
+        forwardRoute={ROUTES.INSTANT_SPLIT}
       />
       <div className={styles.container}>
-        {isFetched && groupData ? (
-          <div className={styles.settings}>
-            <ChangeGroupCurrency
-              groupCode={groupCode}
-              groupCurrency={groupData.group.currency}
-              isOnboarding={true}
-            />
-            <ChangeDataPurgeSetting
-              groupCode={groupCode}
-              inactiveDataPurge={groupData.group.inactiveDataPurge}
-            />
-            <GroupCodeSecurity groupCode={groupCode} />
-          </div>
-        ) : (
+        {!isFetched ? (
           <div className={styles.spinner}>
             <Spinner />
           </div>
+        ) : (
+          groupData && (
+            <div className={styles.settings}>
+              <ChangeGroupCurrency
+                groupCode={groupCode}
+                groupCurrency={groupData.group.currency}
+                isOnboarding
+              />
+              <ChangeDataPurgeSetting
+                groupCode={groupCode}
+                inactiveDataPurge={groupData.group.inactiveDataPurge}
+              />
+              <GroupCodeSecurity groupCode={groupCode} />
+            </div>
+          )
         )}
       </div>
     </main>
