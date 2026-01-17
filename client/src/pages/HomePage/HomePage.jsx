31efcd34504e06db90e6675fcf2544a6b80303b0
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-17 22:00:12 +0100

    refactor: replace useGetActiveGroupCodeOrSetNextInactiveGroupCodeToActiveAndNavigateToMainApplication with useAutoActiveGroupCodeRedirect

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,32 +1,25 @@
-// React and Third-Party Libraries
 import React from "react";
 import { useTranslation } from "react-i18next";
-
-// Hooks
-import useGetActiveGroupCodeOrSetNextInactiveGroupCodeToActiveAndNavigateToMainApplication from "../../hooks/useGetActiveGroupCodeOrSetNextInactiveGroupCodeToActiveAndNavigateToMainApplication";
-
-//Components
 import HelmetMetaTagsNetlify from "../../components/common/HelmetMetaTagsNetlify/HelmetMetaTagsNetlify";
 import PiratePx from "../../components/common/PiratePx/PiratePx";
 import InstantSplitIntroSection from "../../components/features/Home/InstantSplitIntroSection/InstantSplitIntroSection";
 import GetStartedSection from "../../components/features/Home/GetStartedSection/GetStartedSection";
 import TermsAndConditionsSection from "../../components/features/Home/TermsAndConditionsSection/TermsAndConditionsSection";
 import InAppNavigationBar from "../../components/common/InAppNavigation/InAppNavigationBar/InAppNavigationBar";
-
-// Styles
 import styles from "./HomePage.module.css";
+import useAutoActiveGroupCodeRedirect from "../../hooks/useAutoActiveGroupCodeRedirect";
 
 const HomePage = () => {
-  useGetActiveGroupCodeOrSetNextInactiveGroupCodeToActiveAndNavigateToMainApplication();
+  useAutoActiveGroupCodeRedirect();
   const { t } = useTranslation();
 
   return (
     <main>
       <HelmetMetaTagsNetlify title={t("home-page-title")} />
-      <InAppNavigationBar logoOnly={true} />
+      <InAppNavigationBar logoOnly />
       <div className={styles.container}>
         <PiratePx COUNT_IDENTIFIER='homepage' />
-        <h1 className={styles.homepageHeader}>{t("home-page-header")} </h1>
+        <h1 className={styles.homepageHeader}>{t("home-page-header")}</h1>
         <InstantSplitIntroSection />
         <GetStartedSection />
         <TermsAndConditionsSection />
