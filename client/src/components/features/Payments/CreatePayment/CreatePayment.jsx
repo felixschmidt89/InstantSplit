6e1030ad738bbb6d7b6dc5530a09449b6efebf31
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-17 17:23:48 +0100

    refactor: apply ROUTES constant and logic simplification to CreatePayment

--- client/src/components/features/Payments/CreatePayment/CreatePayment.jsx
+++ client/src/components/features/Payments/CreatePayment/CreatePayment.jsx
@@ -1,12 +1,9 @@
-// React and Third-Party Libraries
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Button } from "@mui/material";
 import { IoArrowDownOutline } from "react-icons/io5";
 import { useTranslation } from "react-i18next";
-
-// Constants and Utils
 import { devLog, handleApiErrors } from "../../../../utils/errorUtils";
 import emojiConstants from "../../../../constants/emojiConstants";
 import { buttonStyles } from "../../../../constants/stylesConstants";
@@ -14,31 +11,18 @@ import {
   changeFixedDebitorCreditorOrderSetting,
   deleteAllSettlementsForGroup,
 } from "../../../../utils/settlementUtils";
-
-// Hooks
 import useErrorModalVisibility from "../../../../hooks/useErrorModalVisibility";
-
-// Components
 import PaymentAmountInput from "../PaymentAmountInput/PaymentAmountInput";
 import PaymentMakerSelect from "../PaymentMakerSelect/PaymentMakerSelect";
 import PaymentRecipientSelect from "../PaymentRecipientSelect/PaymentRecipientSelect";
 import ErrorModal from "../../../common/ErrorModal/ErrorModal";
 import Emoji from "../../../common/Emoji/Emoji";
 import RenderReactIcon from "../../../common/RenderReactIcon/RenderReactIcon";
-
-// Styles
+import { ROUTES } from "../../../../constants/routesConstants";
 import styles from "./CreatePayment.module.css";
 
-// API URL
 const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
-/**
- * Parent component for creating a payment.
- *
- * @param {Object} props - The properties of the component.
- * @param {Object[]} props.groupMembers - An array of the group members.
- * @param {string} props.groupCode - The groupCode identifying the group.
- * @returns {JSX.Element} React component. */
 const CreatePayment = ({ groupMembers, groupCode }) => {
   const navigate = useNavigate();
   const { t } = useTranslation();
@@ -48,7 +32,6 @@ const CreatePayment = ({ groupMembers, groupCode }) => {
   const [paymentRecipientName, setPaymentRecipientName] = useState(null);
   const [error, setError] = useState(null);
 
-  // Get error modal visibility logic
   const { isErrorModalVisible, displayErrorModal, handleCloseErrorModal } =
     useErrorModalVisibility();
 
@@ -63,11 +46,12 @@ const CreatePayment = ({ groupMembers, groupCode }) => {
         paymentAmount,
         paymentRecipientName,
       });
+
       devLog("Payment created:", response);
       changeFixedDebitorCreditorOrderSetting(groupCode, false);
       deleteAllSettlementsForGroup(groupCode);
 
-      navigate("/instant-split");
+      navigate(ROUTES.INSTANT_SPLIT);
     } catch (error) {
       if (error.response) {
         handleApiErrors(error, setError, "payments", displayErrorModal, t);
