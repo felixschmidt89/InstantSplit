f4b92d47fb40a11d493f9589a9114e9ef99b3a65
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-17 07:52:19 +0100

    fix: temporarily deactivate resend

--- server/config/adminNotificationEmailConfig.js
+++ server/config/adminNotificationEmailConfig.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { devLog, errorLog } from '../utils/errorUtils.js';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const adminEmailAddresses = {
   sender: 'admin@notifications.instantsplit.de',
