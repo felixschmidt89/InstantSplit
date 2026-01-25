9dfe672b91356c8c7d4cd0075d3058ac170bf219
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-25 18:14:06 +0100

    test: refactor setLocalStorageKey test to use shared mock data and constants

--- client/src/utils/localStorage/setLocalStorageKey.test.js
+++ client/src/utils/localStorage/setLocalStorageKey.test.js
@@ -1,10 +1,12 @@
+import { MOCK_DATA } from "@shared-constants/testConstants";
+import { LOCAL_STORAGE_KEYS } from "@client-constants/localStorageConstants";
 import { setLocalStorageKey } from "./setLocalStorageKey";
 import { debugLog } from "@client-utils/debug/debugLog";
 
 jest.mock("@client-utils/debug/debugLog");
 
 describe("setLocalStorageKey", () => {
-  const mockKey = "testKey";
+  const mockKey = LOCAL_STORAGE_KEYS.ACTIVE_GROUP_CODE;
 
   beforeEach(() => {
     localStorage.clear();
@@ -12,23 +14,25 @@ describe("setLocalStorageKey", () => {
   });
 
   it("should store a string value directly", () => {
-    const value = "stringValue";
-    const result = setLocalStorageKey(mockKey, value);
+    const result = setLocalStorageKey(mockKey, MOCK_DATA.STRING);
 
-    expect(localStorage.getItem(mockKey)).toBe(value);
+    expect(localStorage.getItem(mockKey)).toBe(MOCK_DATA.STRING);
     expect(result).toBe(true);
   });
 
   it("should not stringify null values", () => {
-    setLocalStorageKey(mockKey, null);
+    const result = setLocalStorageKey(mockKey, null);
+
     expect(localStorage.getItem(mockKey)).toBeNull();
+    expect(result).toBe(true);
   });
 
   it("should stringify and store an object value", () => {
-    const value = { id: 1 };
-    const result = setLocalStorageKey(mockKey, value);
+    const result = setLocalStorageKey(mockKey, MOCK_DATA.OBJECT);
 
-    expect(localStorage.getItem(mockKey)).toBe(JSON.stringify(value));
+    expect(localStorage.getItem(mockKey)).toBe(
+      JSON.stringify(MOCK_DATA.OBJECT),
+    );
     expect(result).toBe(true);
   });
 });
