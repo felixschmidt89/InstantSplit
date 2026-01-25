11c6aae29bc0c7f0ce64138f7ae9afb59d1ec332
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-25 18:17:32 +0100

    test: refactor setLocalStorageKey test to delegate null values to deleteLocalStorageKey

--- client/src/utils/localStorage/setLocalStorageKey.test.js
+++ client/src/utils/localStorage/setLocalStorageKey.test.js
@@ -1,8 +1,10 @@
 import { MOCK_DATA } from "@shared-constants/testConstants";
 import { LOCAL_STORAGE_KEYS } from "@client-constants/localStorageConstants";
 import { setLocalStorageKey } from "./setLocalStorageKey";
+import { deleteLocalStorageKey } from "./deleteLocalStorageKey";
 import { debugLog } from "@client-utils/debug/debugLog";
 
+jest.mock("./deleteLocalStorageKey");
 jest.mock("@client-utils/debug/debugLog");
 
 describe("setLocalStorageKey", () => {
@@ -20,10 +22,12 @@ describe("setLocalStorageKey", () => {
     expect(result).toBe(true);
   });
 
-  it("should not stringify null values", () => {
+  it("should delegate to deleteLocalStorageKey when value is null", () => {
+    deleteLocalStorageKey.mockReturnValue(true);
+
     const result = setLocalStorageKey(mockKey, null);
 
-    expect(localStorage.getItem(mockKey)).toBeNull();
+    expect(deleteLocalStorageKey).toHaveBeenCalledWith(mockKey);
     expect(result).toBe(true);
   });
 
@@ -34,5 +38,23 @@ describe("setLocalStorageKey", () => {
       JSON.stringify(MOCK_DATA.OBJECT),
     );
     expect(result).toBe(true);
+  });
+
+  it("should return false and log error on exception", () => {
+    const setItemSpy = jest
+      .spyOn(Storage.prototype, "setItem")
+      .mockImplementation(() => {
+        throw new Error("Storage Full");
+      });
+
+    const result = setLocalStorageKey(mockKey, MOCK_DATA.STRING);
+
+    expect(result).toBe(false);
+    expect(debugLog).toHaveBeenCalledWith(
+      expect.stringContaining("Error setting key"),
+      expect.any(Error),
+    );
+
+    setItemSpy.mockRestore();
   });
 });
