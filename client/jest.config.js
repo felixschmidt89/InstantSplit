df086763f6d5c14d161416843f4992de38baa2db
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-25 15:25:02 +0100

    refactor: update jest configuration to include svg transformer and enhance module name mapping

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,9 +3,11 @@ export default {
 
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.svg$": "jest-transformer-svg",
   },
 
   moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "^@client-utils/(.*)$": "<rootDir>/src/utils/$1",
     "^@client-constants/(.*)$": "<rootDir>/src/constants/$1",
     "^@client-hooks/(.*)$": "<rootDir>/src/hooks/$1",
