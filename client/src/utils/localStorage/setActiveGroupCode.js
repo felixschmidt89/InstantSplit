65fb27b9e9bed3383defea95a095e6c035fd5c6e
felixschmidt89 <felix.schmidt@protonmail.com>
2026-01-25 18:52:34 +0100

    refactor: simplify setActiveGroupCode by using setLocalStorageKey

--- client/src/utils/localStorage/setActiveGroupCode.js
+++ client/src/utils/localStorage/setActiveGroupCode.js
@@ -1,13 +1,6 @@
 import { LOCAL_STORAGE_KEYS } from "@client-constants/localStorageConstants";
-import { debugLog } from "@client-utils/debug/debugLog";
+import { setLocalStorageKey } from "./setLocalStorageKey";
 
 export const setActiveGroupCode = (groupCode) => {
-  try {
-    localStorage.setItem(LOCAL_STORAGE_KEYS.ACTIVE_GROUP_CODE, groupCode);
-    debugLog("GroupCode set to active:", groupCode);
-    return true;
-  } catch (error) {
-    debugLog("Error setting groupCode to active", error);
-    return false;
-  }
+  return setLocalStorageKey(LOCAL_STORAGE_KEYS.ACTIVE_GROUP_CODE, groupCode);
 };
