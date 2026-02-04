c988a9c498ba2c1c81e8a9919eadfb76be23e41e
felixschmidt89 <felix.schmidt@protonmail.com>
2026-02-04 21:09:33 +0100

    refactor: reorganize imports and enhance debug logging in useTriggerRerender hook

--- client/src/constants/errorConstants.jsx
+++
@@ -1,7 +0,0 @@
-/**
- * Generic error message constant.
- *
- * @constant
- * @type {string}
- */
-export const genericErrorMessage = "An error occurred. Please try again later.";
--- client/src/hooks/useTriggerRerender.jsx
+++ client/src/hooks/useTriggerRerender.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
+
 import { getActiveGroupCode } from "../utils/localStorage";
-import { debugLog } from "../../../shared/utils/debug";
+
+import { debugLog } from "../../../shared/utils/debug/debugLog.js";
+import { LOG_LEVELS } from "../../../shared/constants/debugConstants.js";
 
 const useTriggerRerender = () => {
   const groupCode = getActiveGroupCode();
-
   const [rerenderTrigger, setRerenderTrigger] = useState(1);
 
   const incrementRerenderTrigger = () => {
+    debugLog(
+      "Incrementing rerender trigger",
+      { currentTrigger: rerenderTrigger },
+      LOG_LEVELS.INFO,
+    );
+
     setRerenderTrigger((prevValue) => prevValue + 1);
-    debugLog("Rerender trigger incremented:", rerenderTrigger);
   };
 
   return {
