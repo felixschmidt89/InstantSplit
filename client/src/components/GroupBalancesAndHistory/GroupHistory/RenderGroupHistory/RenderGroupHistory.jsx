bca319b5a75c9fc7d7cbf19eb55d8a352b585f23
felixschmidt89 <felix.schmidt@protonmail.com>
2026-02-04 18:45:13 +0100

    fix: remove unnecessary dependency from useEffect in RenderGroupHistory

--- client/src/components/GroupBalancesAndHistory/GroupHistory/RenderGroupHistory/RenderGroupHistory.jsx
+++ client/src/components/GroupBalancesAndHistory/GroupHistory/RenderGroupHistory/RenderGroupHistory.jsx
@@ -67,7 +67,7 @@ const RenderGroupHistory = ({ groupCode, groupCurrency }) => {
     };
 
     fetchGroupExpensesAndPayments();
-  }, [groupCode, t, displayErrorModal]);
+  }, [groupCode, t]);
 
   if (isLoading) {
     return (
