b002891d614d90757109e1387b9a73153ecc1b36
felixschmidt89 <felix.schmidt@protonmail.com>
2026-02-06 09:40:36 +0100

    refactor: simplify required field validations in payment schema

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -4,23 +4,23 @@ const paymentSchema = new Schema(
   {
     paymentAmount: {
       type: Number,
-      required: [true, 'Missing payment amount'],
-      min: [0.01, 'The payment amount must be at least 0.01.'],
-      max: [99999.99, 'The payment amount may not exceed 99999.99'],
+      required: true,
+      min: 0.01,
+      max: 99999.99,
     },
     paymentMaker: {
       type: Schema.Types.ObjectId,
       ref: 'User',
-      required: [true, 'Missing ID of the person who made the payment'],
+      required: true,
     },
     paymentRecipient: {
       type: Schema.Types.ObjectId,
       ref: 'User',
-      required: [true, 'Missing ID of the person who received the payment'],
+      required: true,
     },
     groupCode: {
       type: String,
-      required: [true, 'Missing groupCode'],
+      required: true,
     },
   },
   {
