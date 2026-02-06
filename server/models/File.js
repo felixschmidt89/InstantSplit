d08d46dfc02634917869884891025c63f58e8e8a
felixschmidt89 <felix.schmidt@protonmail.com>
2026-02-06 09:11:35 +0100

    refactor: ensure required and trim validations for file schema fields

--- server/models/File.js
+++ server/models/File.js
@@ -4,18 +4,27 @@ const fileSchema = new Schema(
   {
     filename: {
       type: String,
+      required: true,
+      trim: true,
     },
     path: {
       type: String,
+      required: true,
+      trim: true,
     },
     mimetype: {
       type: String,
+      required: true,
+      trim: true,
     },
     size: {
       type: Number,
+      required: true,
     },
     cloudinaryURL: {
       type: String,
+      required: true,
+      trim: true,
     },
   },
   { timestamps: true },
