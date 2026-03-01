55e03ec93efa00f0a1eafe1f8b18515b1ff9fba5
felixschmidt89 <felix.schmidt@protonmail.com>
2026-03-01 20:15:45 +0100

    fix: instant split logo link creates 404

--- client/src/components/InstantSplitLogo/InstantSplitLogo.jsx
+++ client/src/components/InstantSplitLogo/InstantSplitLogo.jsx
@@ -1,21 +1,30 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+
 import { ROUTES } from "../../constants/routesConstants";
+
 import styles from "./InstantSplitLogo.module.css";
 
-const InstantSplitLogo = ({ linkToInstantSplitPage = true }) => {
+const { INSTANT_SPLIT } = ROUTES;
+const LOGO_PATH = "/logo_coloured.svg";
+
+const InstantSplitLogo = ({ isLink = true }) => {
   const logoImage = (
-    <img src='/logo_coloured.svg' alt='Logo' className={styles.logoImage} />
+    <img
+      src={LOGO_PATH}
+      alt='Instant Split Logo'
+      className={styles.logoImage}
+    />
   );
 
   return (
     <>
       <Helmet>
-        <link rel='preload' href='/logo_coloured.svg' as='image' />
+        <link rel='preload' href={LOGO_PATH} as='image' />
       </Helmet>
-      {linkToInstantSplitPage ? (
-        <Link to={ROUTES.INSTANT_SPLIT} className={styles.logoLink}>
+
+      {isLink ? (
+        <Link to={`/${INSTANT_SPLIT}`} className={styles.logoLink}>
           {logoImage}
         </Link>
       ) : (
