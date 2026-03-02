bdde0d2b8f88f3bcc84045eba69fb680b19fc4af
felixschmidt89 <felix.schmidt@protonmail.com>
2026-03-02 19:46:22 +0100

    feat: set pwa reshow treshold to to seven day and polling interval to 30 seconds

--- shared/constants/applicationConstants.js
+++ shared/constants/applicationConstants.js
@@ -1,3 +1,3 @@
-export const PWA_PROMPT_RESHOW_THRESHOLD_MS = 24 * 60 * 60 * 1000; // 24 hours
+export const PWA_PROMPT_RESHOW_THRESHOLD_MS = 7 * 24 * 60 * 60 * 1000; // 7 days
 
-export const POLLING_INTERVAL_MS = 10 * 1000; // 10 seconds
+export const POLLING_INTERVAL_MS = 30 * 1000; // 30 seconds
