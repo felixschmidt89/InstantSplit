d2c8209c283f8cccb5d089defab4d55706a78ceb
felixschmidt89 <felix.schmidt@protonmail.com>
2026-04-03 18:34:51 +0200

    refactor: simplify settlement calculation logic and move it to server

--- server/controllers/settlement/calculateSettlementsController.js
+++ server/controllers/settlement/calculateSettlementsController.js
@@ -1,40 +1,14 @@
-import { StatusCodes } from 'http-status-codes';
-import calculateSettlementsService from '../../services/settlement/calculateSettlementsService.js';
-import PAYLOAD_KEYS from '../../../shared/constants/api/payloadKeyConstants.js';
-import ERROR_CODES from '../../../shared/constants/system/errorConstants.js';
-
-const { CREATED, BAD_REQUEST } = StatusCodes;
-const { SETTLEMENT_KEYS } = PAYLOAD_KEYS;
-const { SETTLEMENT_ERRORS } = ERROR_CODES;
-
 const calculateSettlementsController = async (req, res, next) => {
   try {
-    const { [SETTLEMENT_KEYS.SETTLEMENTS]: settlements } = req.body;
     const { groupCode } = req.context;
 
-    const hasValidSettlements = Boolean(
-      Array.isArray(settlements) && settlements.length > 0,
-    );
+    const settlements = await calculateSettlementsService(groupCode);
 
-    if (!hasValidSettlements) {
-      return res.status(BAD_REQUEST).json({
-        success: false,
-        error: SETTLEMENT_ERRORS.INVALID_ARRAY,
-      });
-    }
-
-    const savedSettlements = await calculateSettlementsService(
-      groupCode,
-      settlements,
-    );
-
-    return res.status(CREATED).json({
+    return res.status(StatusCodes.CREATED).json({
       success: true,
-      data: { settlements: savedSettlements },
+      data: { settlements },
     });
   } catch (error) {
     next(error);
   }
 };
-
-export default calculateSettlementsController;
--- server/services/settlement/calculateSettlementsService.js
+++ server/services/settlement/calculateSettlementsService.js
@@ -1,32 +1,77 @@
+import Member from '../../models/Member.js';
 import Settlement from '../../models/Settlement.js';
 import Group from '../../models/Group.js';
-import PAYLOAD_KEYS from '../../../shared/constants/api/payloadKeyConstants.js';
 import SETTLEMENT from '../../../shared/constants/models/settlementConstants.js';
+import GROUP from '../../../shared/constants/models/groupConstants.js';
 import COMMON from '../../../shared/constants/models/commonConstants.js';
+import SYSTEM from '../../../shared/constants/system/systemConstants.js';
 
-const { SETTLEMENT_KEYS } = PAYLOAD_KEYS;
 const { SETTLEMENT_FIELDS } = SETTLEMENT;
+const { GROUP_FIELDS } = GROUP;
 const { COMMON_FIELDS } = COMMON;
+const { BALANCE_THRESHOLD } = SYSTEM;
 
-// TODO: Needs improvement later
-const calculateSettlementsService = async (groupCode, settlements) => {
-  const settlementsWithGroup = settlements.map((settlement) => ({
-    [SETTLEMENT_FIELDS.DEBTOR]: settlement[SETTLEMENT_KEYS.DEBTOR],
-    [SETTLEMENT_FIELDS.CREDITOR]: settlement[SETTLEMENT_KEYS.CREDITOR],
-    [SETTLEMENT_FIELDS.AMOUNT]: settlement[SETTLEMENT_KEYS.AMOUNT],
+const calculateSettlementsService = async (groupCode) => {
+  const members = await Member.find({
     [COMMON_FIELDS.GROUP_CODE]: groupCode,
-  }));
+  }).lean();
+
+  let debtors = members
+    .filter((member) => member.balance <= -BALANCE_THRESHOLD)
+    .map((member) => ({
+      id: member._id,
+      balance: Math.abs(member.balance),
+    }));
+
+  let creditors = members
+    .filter((member) => member.balance >= BALANCE_THRESHOLD)
+    .map((member) => ({
+      id: member._id,
+      balance: member.balance,
+    }));
+
+  const calculatedSettlements = [];
+
+  let debtorIndex = 0;
+  let creditorIndex = 0;
+
+  while (debtorIndex < debtors.length && creditorIndex < creditors.length) {
+    const amount = Math.min(
+      debtors[debtorIndex].balance,
+      creditors[creditorIndex].balance,
+    );
+
+    if (amount >= BALANCE_THRESHOLD) {
+      calculatedSettlements.push({
+        [SETTLEMENT_FIELDS.DEBTOR]: debtors[debtorIndex].id,
+        [SETTLEMENT_FIELDS.CREDITOR]: creditors[creditorIndex].id,
+        [SETTLEMENT_FIELDS.AMOUNT]: Number(amount.toFixed(2)),
+        [COMMON_FIELDS.GROUP_CODE]: groupCode,
+      });
+    }
+
+    debtors[debtorIndex].balance -= amount;
+    creditors[creditorIndex].balance -= amount;
+
+    if (debtors[debtorIndex].balance < BALANCE_THRESHOLD) {
+      debtorIndex++;
+    }
+
+    if (creditors[creditorIndex].balance < BALANCE_THRESHOLD) {
+      creditorIndex++;
+    }
+  }
 
   await Settlement.deleteMany({ [COMMON_FIELDS.GROUP_CODE]: groupCode });
 
-  const savedSettlements = await Settlement.insertMany(settlementsWithGroup);
+  const settlements = await Settlement.insertMany(calculatedSettlements);
 
   await Group.updateOne(
     { [COMMON_FIELDS.GROUP_CODE]: groupCode },
-    { $set: { settlementsCalculated: true } },
+    { $set: { [GROUP_FIELDS.SETTLEMENTS_CALCULATED]: true } },
   );
 
-  return savedSettlements;
+  return settlements;
 };
 
 export default calculateSettlementsService;
